1f09603acf0f482ecb83b604d0ea7b714f3903cb
Ruslan Zakirov <ruz@bestpractical.com>
2009-01-12 21:31:16 +0000

    initial commit

---
+++ lib/RT/Extension/MergeUsersHistory.pm
@@ -0,0 +1,28 @@
+use 5.008003;
+use strict;
+use warnings;
+
+package RT::Extension::MergeUsersHistory;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT::Extension::MergeUsersHistory - merge historical activity of RT users.
+
+=head1 DESCRIPTION
+
+This distribution is shipped with a command line script F<sbin/rt-merge-users-history>,
+run it without arguments to get usage details.
+
+=head1 AUTHOR
+
+Ruslan Zakirov E<lt>ruz@bestpractical.comE<gt>
+
+=head1 LICENSE
+
+Under the same terms as Perl itself.
+
+=cut
+
+1;
